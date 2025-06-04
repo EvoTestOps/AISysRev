c3abd71ce5d7eb2a352ffd23d5f3197d2397d25d
opturtio <olli-pekka.turtio@helsinki.fi>
2025-06-04 16:51:54 +0300

    connection to backend created. files can be send to backend.

--- client/src/services/fileUploadService.ts
+++ client/src/services/fileUploadService.ts
@@ -4,11 +4,12 @@ export const fileUploadToBackend = async (files: File[]) => {
   const formData = new FormData();
 
   files.forEach((file) =>
-    formData.append("file", file)
+    formData.append("files", file)
   );
 
   try {
-    const res = await axios.post('/api/validate-csv', formData);
+    const res = await axios.post('/api/process-csv', formData);
+    console.log("Upload successful:", res.data);
     return res.data;
   } catch (error){
     console.error("Backend upload error", error);
--- server/src/main.py
+++ server/src/main.py
@@ -1,7 +1,8 @@
 import uvicorn
 import dotenv
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from typing import List
 
 dotenv.load_dotenv()
 
@@ -13,7 +14,6 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 app = FastAPI()
 
-
 @app.on_event("startup")
 def on_startup():
     print("on_startup hook called")
@@ -23,6 +23,10 @@ def on_startup():
 def health_check():
     return {"status": "ok"}
 
+@app.post("/api/process-csv")
+async def process_csv(files: List[UploadFile] = File(...)):
+    filenames = [file.filename for file in files]
+    return {"status": "received", "filenames": filenames}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8080, host="0.0.0.0", reload=True)
