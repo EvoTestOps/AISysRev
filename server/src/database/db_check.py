8c57064426edbcb426c061a27c01a396b25960fb
opturtio <olli-pekka.turtio@helsinki.fi>
2025-06-11 17:42:04 +0300

    split db_check and engine to their own files

--- server/src/database/db_check.py
+++ server/src/database/db_check.py
@@ -1,25 +1,17 @@
-from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy import text
-import os
-from dotenv import load_dotenv
-from sqlalchemy.engine.url import make_url
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.engine.url import make_url
 
-load_dotenv()
+from database.engine import engine
 
-DB_URL = os.getenv("DB_URL")
-
-engine = create_async_engine(DB_URL, echo=True)
-url = make_url(DB_URL)
-print(f'Dialect: {url.get_dialect().__module__}')
+url = make_url(engine.url)
+print(f"Dialect: {url.get_dialect().__module__}")
 
 async def check_database_connection():
     try:
         async with engine.connect() as conn:
             result = await conn.execute(text("SELECT * FROM pg_database"))
-            rows = result.fetchall()
-            print("Database check successful. Databases:")
-            for row in rows:
-                print(row)
+            result.fetchall()
+            print("Database check successful.")
     except SQLAlchemyError as e:
-        print(f'Database connection failed: {e}')
+        print(f"Database connection failed: {e}")
