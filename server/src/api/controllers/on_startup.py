05d8d226510fd28198d77266e756903ba194d114
opturtio <olli-pekka.turtio@helsinki.fi>
2025-07-08 13:38:50 +0300

    Add migration function to on_startup process

--- server/src/api/controllers/on_startup.py
+++ server/src/api/controllers/on_startup.py
@@ -1,9 +1,22 @@
 import os
+import asyncio
 import redis.asyncio as redis
 from fastapi import APIRouter
+from alembic import command
+from alembic.config import Config
 from src.db.db_check import check_database_connection
 
 router = APIRouter()
+
+async def run_migration():
+    alembic_cfg = Config(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'alembic.ini'))
+    loop = asyncio.get_running_loop()
+    try:
+        await loop.run_in_executor(None, command.upgrade, alembic_cfg, "head")
+    except Exception as e:
+        print(f"Error during migration: {e}")
+        raise
+
 
 async def check_redis_connection():
     redis_url = os.getenv("REDIS_URL")
@@ -23,5 +36,6 @@ async def check_redis_connection():
 @router.on_event("startup")
 async def on_startup():
     print("on_startup hook called")
+    await run_migration()
     await check_database_connection()
     await check_redis_connection()
