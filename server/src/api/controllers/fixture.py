7d5d006a827391f2d4fc28c1149f6d48ff241f4e
Aleksi Huotala <7612995+alehuo@users.noreply.github.com>
2025-07-11 21:47:57 +0300

    Create Fixture router; The fixture router is used to reset server fixtures.

---
+++ server/src/api/controllers/fixture.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, HTTPException, status
+import os
+from src.db.engine import engine
+from src.db.session import Base
+
+router = APIRouter()
+
+
+@router.post("/api/v1/fixtures/reset")
+async def reset_fixtures():
+    try:
+        test_mode = os.getenv("TEST_MODE", False)
+        if not test_mode:
+            # Lets return HTTP 404 so it behaves like a non-existing route
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+        return {"status": "ok"}
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to fetch job: {str(e)}",
+        )
