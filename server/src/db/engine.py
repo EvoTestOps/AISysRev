d264c287cecf514fa3d6273757a6c0a4816245de
opturtio <olli-pekka.turtio@helsinki.fi>
2025-08-02 17:46:09 +0300

    Add SQLAlchemy logging. Logging set to DEBUG

--- server/src/db/engine.py
+++ server/src/db/engine.py
@@ -1,7 +1,11 @@
+import os
+import logging
 from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine
 from src.core.config import settings
-import os
+
+logging.basicConfig()
+logging.getLogger("sqlalchemy.engine").setLevel(logging.DEBUG)
 
 DB_URL = settings.DB_URL
 if not DB_URL:
