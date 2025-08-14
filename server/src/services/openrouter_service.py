d1334b8306bc666263ed2a7b6896790f22727c61
Aleksi Huotala <7612995+alehuo@users.noreply.github.com>
2025-08-14 11:56:48 +0300

    Prepare for calling LLM from the backend

--- server/src/services/openrouter_service.py
+++ server/src/services/openrouter_service.py
@@ -1,3 +1,6 @@
+from src.schemas.llm import LLMConfiguration
+from src.core.llm import OpenRouterLLM
+from src.core.config import settings
 from src.models.openrouter import OpenrouterModelResponse
 
 
@@ -9,8 +12,15 @@ class OpenRouterService:
             parsed = json.load(f)
             return parsed
 
-    def call_llm(self):
-        return None
+    async def call_llm(self):
+        llm_configuration = LLMConfiguration(
+            base_url=settings.LLM_PROVIDER_BASE_URL, api_key=""
+        )
+        llm = OpenRouterLLM(config=llm_configuration)
+        response_formatted, response_raw = await llm.generate_answer_async(
+            "PROMPT GOES HERE"
+        )
+        return response_formatted
 
 
 def get_openrouter_service() -> OpenRouterService:
