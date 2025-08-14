535d7fa3c83f4e124c4d5a0ecac49260b27f41ac
opturtio <olli-pekka.turtio@helsinki.fi>
2025-08-14 22:20:08 +0300

    Add unique constraint and foreign key for file_uuid in Paper model

--- server/src/models/paper.py
+++ server/src/models/paper.py
@@ -11,11 +11,12 @@ class Paper(Base, TimestampMixin):
     uuid = Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
     paper_id = Column(Integer, nullable=False)
     project_uuid = Column(UUID(as_uuid=True), ForeignKey("project.uuid", ondelete="CASCADE"), nullable=False)
-    file_uuid = Column(UUID(as_uuid=True), nullable=False)
+    file_uuid = Column(UUID(as_uuid=True), ForeignKey("file.uuid", ondelete="CASCADE"), nullable=False)
     doi = Column(Text, nullable=False)
     title = Column(Text, nullable=False)
     abstract = Column(Text, nullable=False)
 
     __table_args__ = (
         UniqueConstraint('project_uuid', 'paper_id', name='uq_project_paper_id'),
+        UniqueConstraint('file_uuid', name='uq_file_uuid'),
     )
