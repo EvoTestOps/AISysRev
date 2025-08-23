6b8146256a4cacaf5b66c6bce70aa4330f91bad7
Aleksi Huotala <7612995+alehuo@users.noreply.github.com>
2025-08-23 18:00:29 +0300

    WIP: subscribe functionality

--- server/src/redis/client.py
+++ server/src/redis/client.py
@@ -1,7 +1,18 @@
 from src.core.config import settings
 import redis
+import json
+
 
 def get_redis_client():
     redis_url = settings.REDIS_URL
     client = redis.from_url(redis_url)
     return client
+
+# Subscribes to job task events, which we publish to the front-end
+def subscribe(client: redis.Redis):
+    pubsub = client.pubsub()
+    pubsub.subscribe("job_task")
+    for message in pubsub.listen():
+        if message.get("type") == "message":
+            data = json.loads(message.get("data"))
+            print(data)
