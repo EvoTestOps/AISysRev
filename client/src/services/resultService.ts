8a52a20b0c9fcd90b6249a67fe3aab915b243858
Aleksi Huotala <7612995+alehuo@users.noreply.github.com>
2025-09-04 10:44:22 +0300

    Use URLSearchParams to format query params

--- client/src/services/resultService.ts
+++ client/src/services/resultService.ts
@@ -1,6 +1,8 @@
-import { api } from '../services/api'
+import { api } from "../services/api";
 
 export const fetchResultFromBackend = async (projectUuid: string) => {
-  const res = await api.get(`/result/?project_uuid=${projectUuid}`);
+  const res = await api.get(
+    `/result/?${new URLSearchParams({ project_uuid: projectUuid }).toString()}`
+  );
   return res.data;
 };
